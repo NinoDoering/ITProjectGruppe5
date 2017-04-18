ac5a74a521754e27ef8c7e2716a94db5d3f83ffb
WahidVanaki <wahidvanaki@10.60.53.25>
2017-04-18 17:13:51 +0200

    Neue Ausgabe

--- src/test.java
+++ src/test.java
@@ -9,6 +9,8 @@ public class test {
 		System.out.println("hallo leute wie gehts euch AN diesem Donnerstag blablabla ");
 
 		System.out.println("hallo ich bins Tony");
+		
+		System.out.println("Hallo");
 
 		
 	}
