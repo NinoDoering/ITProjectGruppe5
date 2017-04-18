63c0adf0f6bcedc29af73a1480aeae73b704f1e2
WahidVanaki <wahidvanaki@10.60.53.25>
2017-04-18 17:37:19 +0200

    Neue Angabe

--- src/test.java
+++ src/test.java
@@ -9,10 +9,12 @@ public class test {
 		System.out.println("hallo leute wie gehts euch AN diesem Donnerstag blablabla ");
 
 		System.out.println("hallo ich bins Tony");
-		
-		System.out.println("Hallo");
 
+		System.out.println("Hallo");
 		
+		int a = 21;
+		
+		int t = 22;
 	}
 
 }
